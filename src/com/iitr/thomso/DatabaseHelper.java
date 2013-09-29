4777599cd2731717e5bbc29d62a7b9f31c28e1c4
kishorm23 <kishormohite888@gmail.com>
2013-09-29 17:13:53 +0530

    added modified database

--- src/com/iitr/thomso/DatabaseHelper.java
+++ src/com/iitr/thomso/DatabaseHelper.java
@@ -5,12 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import android.R.string;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
