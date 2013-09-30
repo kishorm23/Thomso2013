f8c0abe572cce4069e9f0a1bfa088f53a1cc377e
kishorm23 <kishormohite888@gmail.com>
2013-09-30 14:49:11 +0530

    Fixes app crash on AM PM conversion problem

--- src/com/iitr/thomso/Day0.java
+++ src/com/iitr/thomso/Day0.java
@@ -1,6 +1,7 @@
 package com.iitr.thomso;
 
 import java.io.IOException;
+
 import java.util.ArrayList;
 
 import java.util.HashMap;
@@ -14,32 +15,24 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.app.SherlockListFragment;
 
 @SuppressLint("ValidFragment")
 public class Day0 extends SherlockListFragment {
 	
+	String[][] Data;
 	SharedPreferences somedata;
 	String radio = "SharedString";
 	
-	//Retrieve from database
-	String[][] Data;
-	Spinner spinner;
-	 private String array_spinner[];
-	LinearLayout llayout;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		List<HashMap<String , String  >> eventList = new ArrayList<HashMap<String , String >>();
 		DatabaseHelper myDbHelper = new DatabaseHelper(getSherlockActivity().getBaseContext());
 		try{
@@ -53,27 +46,19 @@ public class Day0 extends SherlockListFragment {
 		catch(SQLException sqle){
 				throw sqle;
 			}
-		SimpleAdapter adapter = null;
 		somedata = getSherlockActivity().getSharedPreferences(radio, 0);
 		String dataReturned = somedata.getString("filter", "All");
 		if(dataReturned=="All") Data = myDbHelper.getEventbyDay(0);
 		else Data = myDbHelper.getEventbyType(dataReturned, 0);
-		if(Data.length!=0)
-		{
+		
 		for(int i=0;i<Data.length;i++){
-			String x = null;
-			try
-			{x = ConvertTo(Data[i][4]);
-			if(Data[i][5]!=null) x=x+" - "+ConvertTo(Data[i][5]);
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
+			String x=Data[i][4];
+			Log.i("DEBUG_TIME","Current Event:"+Data[i][2]);
+			x=ConvertTo(Data[i][4]);
             HashMap<String, String> hm = new HashMap<String,String>();
             hm.put("name", "" + Data[i][2]);
             hm.put("venue","" + "Venue: "+Data[i][3]);
-            hm.put("time","" +  x );
+            hm.put("time","" + "Starts at: " + x );
             eventList.add(hm);
             
             // Keys used in Hashmap
@@ -83,33 +68,13 @@ public class Day0 extends SherlockListFragment {
             int[] to = { R.id.eName,R.id.eVenue,R.id.eTime};
             
             // Instantiating an adapter to store each items
-            // R.layout.listview_layout defines the layout of each ite
-            adapter = new SimpleAdapter(getActivity().getBaseContext(), 
+            // R.layout.listview_layout defines the layout of each item
+            SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), 
             				eventList, R.layout.listview_layout, from, to);
      
             // Setting the adapter to the listView
-		}
-		}
-		else
-		{
-			HashMap<String, String> hm = new HashMap<String,String>();
-            hm.put("name", "" + "");
-            hm.put("venue","" + "No event in this catagory");
-            hm.put("time","" + "" );
-            eventList.add(hm);
-            String[] from = { "name","venue","time" };
-            
-            //Ids of views in listview_layout
-            int[] to = { R.id.eName,R.id.eVenue,R.id.eTime};
-            
-            // Instantiating an adapter to store each items
-            // R.layout.listview_layout defines the layout of each ite
-            adapter = new SimpleAdapter(getActivity().getBaseContext(), 
-            				eventList, R.layout.listview_layout, from, to);
-            
-			
-		}
-            setListAdapter(adapter);
+            setListAdapter(adapter);   
+        }
 		return super.onCreateView(inflater, container, savedInstanceState); 
 	}
 	
@@ -128,32 +93,28 @@ public class Day0 extends SherlockListFragment {
 	        };
 	 
 	        getListView().setOnItemClickListener(listener);
-	        if(Data.length==0) getListView().setEnabled(false);
 	 
 	    }
 	 
-	 
 		public String ConvertTo(String time)
 		{
-			Log.i("DEBUG_TIME","At "+ time );
-			if(time.charAt(0)=='9') time="0"+time;
+			if(time.length()==4) time="0"+time;
 			String hours = (String) time.subSequence(0, 2);
 			String minutes = (String) time.subSequence(3, 5);
+			
 			
 			int hour = Integer.parseInt(hours);
 			int minute = Integer.parseInt(minutes);
 			
 			String toReturn;
 			if(hour>12) toReturn=String.valueOf(hour-12);
-			else if(hour<10) toReturn = String.valueOf(hour);
 			else toReturn=hours;
 			if(minute<10) toReturn=toReturn+":"+"0"+minute;
 			else toReturn=toReturn+":"+minute;
 			if(hour>12) toReturn = toReturn + " PM";
-			else toReturn=toReturn+"AM";
+			else toReturn=toReturn+" AM";
 			Log.i("DEBUG_TIME","hour: "+hour+" minutes: "+minutes+" Converted String: "+toReturn);
 			return toReturn;
 		}
-	
 	
 }
--- src/com/iitr/thomso/Day1.java
+++ src/com/iitr/thomso/Day1.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.database.SQLException;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,7 +26,8 @@ import com.actionbarsherlock.app.SherlockListFragment;
 public class Day1 extends SherlockListFragment {
 	
 	String[][] Data;
-	
+	SharedPreferences somedata;
+	String radio = "SharedString";
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,14 +46,19 @@ public class Day1 extends SherlockListFragment {
 		catch(SQLException sqle){
 				throw sqle;
 			}
-		Data=myDbHelper.getEventbyDay(1);
+		somedata = getSherlockActivity().getSharedPreferences(radio, 0);
+		String dataReturned = somedata.getString("filter", "All");
+		if(dataReturned=="All") Data = myDbHelper.getEventbyDay(1);
+		else Data = myDbHelper.getEventbyType(dataReturned, 1);
 		
 		for(int i=0;i<Data.length;i++){
-			//String x = ConvertTo(Data[i][4]);
+			String x=Data[i][4];
+			Log.i("DEBUG_TIME","Current Event:"+Data[i][2]);
+			x=ConvertTo(Data[i][4]);
             HashMap<String, String> hm = new HashMap<String,String>();
             hm.put("name", "" + Data[i][2]);
             hm.put("venue","" + "Venue: "+Data[i][3]);
-            hm.put("time","" + "Starts at: " + Data[i][4] );
+            hm.put("time","" + "Starts at: " + x );
             eventList.add(hm);
             
             // Keys used in Hashmap
@@ -91,6 +98,7 @@ public class Day1 extends SherlockListFragment {
 	 
 		public String ConvertTo(String time)
 		{
+			if(time.length()==4) time="0"+time;
 			String hours = (String) time.subSequence(0, 2);
 			String minutes = (String) time.subSequence(3, 5);
 			
@@ -100,12 +108,11 @@ public class Day1 extends SherlockListFragment {
 			
 			String toReturn;
 			if(hour>12) toReturn=String.valueOf(hour-12);
-			else if(hour<10) toReturn = "0"+hour;
 			else toReturn=hours;
 			if(minute<10) toReturn=toReturn+":"+"0"+minute;
 			else toReturn=toReturn+":"+minute;
 			if(hour>12) toReturn = toReturn + " PM";
-			else toReturn=toReturn+"AM";
+			else toReturn=toReturn+" AM";
 			Log.i("DEBUG_TIME","hour: "+hour+" minutes: "+minutes+" Converted String: "+toReturn);
 			return toReturn;
 		}
